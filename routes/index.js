2bce857d84799d990c74b6d4a7f73c3398ced438
Nathan <dewakik@gmail.com>
2020-05-17 22:48:42 +0200

    [EDIT] Convert to ejs

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// PUG VARIANT
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
 /* GET about page */
-router.get('/about', function(req, res) {
-  res.render('about', {title: 'About me', name:'Nathan'});
-  router.use(express.static('public'));
-});
+// router.get('/about', function(req, res) {
+//   res.render('about', {title: 'About me', name:'Nathan'});
+//   router.use(express.static('public'));
+// });
 
 /* Route Path to  */
+
+
+router.get('/', (req, res) => {
+  res.render('index', {title: 'my life'});
+});
+
 
 module.exports = router;
