b5267441d17da88e98f2807edc59bfbcf4e5aef4
Nathan <dewakik@gmail.com>
2020-05-29 00:30:53 +0200

    [EDIT] fix issue of DRY but it removed my view of input data

--- route/mainRouter.js
+++ route/mainRouter.js
@@ -24,16 +24,10 @@ router.get('/about', (req, res) => {
 /**
  * @source https://expressjs.com/en/4x/api.html#router
  *  */
-
 router.get('/feature', (req, res, next) => {
-    const minAge = req.body.minAge;
-    const maxAge = req.body.maxAge;
-    const distance = req.body.distance;
-    const sexPref = req.body.sexPref;
   res.render('feature', {
     title: 'prefences',
     filterData: data.users,
-    minAge: minAge, maxAge: maxAge, distance: distance, sexPref: sexPref,
 
   });
 });
