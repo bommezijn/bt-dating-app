b90dfda44779217beef75cc77259eef504c4ba27
Nathan <nathanbommezijn@gmail.com>
2020-05-30 21:05:13 +0200

    [UPDATE] Added notes of what went wrong.

--- route/addUser.js
+++ route/addUser.js
@@ -2,6 +2,15 @@ const express = require('express');
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
+/* The fix for this was putting mainrouter on the end
+  - downright removing the same router from mainRouter
+  - adding correct routing of creating user
+  - fixing the form action to ./user
+  - explaining that my 404 is hijacking the route for a moment
+  - VERY ANNOYING: How I named my files and routes.
+  - In principle you want a route for each element of a CRUD.
+   */
+
 router.get('/user', (req, res, next) => {
       console.log('abdel')
       res.render('partial/addUser', {
