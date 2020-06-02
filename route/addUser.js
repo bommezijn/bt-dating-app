efc503d66007b94ee547ac0afe980a4baebdc372
Nathan <dewakik@gmail.com>
2020-06-02 16:59:50 +0200

    [UPDATE] Break, Can't delete
    When clicking delete sends to 404 cant find page /add/allUsers

--- route/addUser.js
+++ route/addUser.js
@@ -2,13 +2,11 @@
  * @file Managing routes for add/
  * @description Manages routing and data retrieval from db for fake user data.
  * Also used source for coloring the console.
-  * \x1b[33m = foreground yellow,
-  * \x1b[0m = reset instance so the rest isnt yellow either
+ * \x1b[33m = foreground yellow,
+ * \x1b[0m = reset instance so the rest isnt yellow either
  * @source https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-
  * @author Nathan Bommezijn
  */
-
-
 /* The fix for this was putting mainrouter on the end
   - downright removing the same router from mainRouter
   - adding correct routing of creating user
@@ -30,11 +28,21 @@ const ObjectId = require('mongodb').ObjectId;
  * See {@link dbFile}
  * @source https://dev.to/lenmorld/rest-api-with-mongodb-atlas-cloud-node-and-express-in-10-minutes-2ii1
  */
+db.initialize(dbName, collectionName, function(dbCollection) { // successCallback
+  // get all items
+  dbCollection.find().toArray(function(err, result) {
+    if (err) throw err;
+    console.log(result);
 
-router.get('/user', (req, res, next) => {
-  console.log('Entered add/user');
-  db.initialize(dbName, 'genres', function(dbCollection) {
-    dbCollection.findOne({num_id: 1}, (error, result) => {
+    // << return response to client >>
+  });
+
+
+  router.get('/user', (req, res, next) => {
+    console.log('Entered add/user');
+    dbCollection.findOne({
+      num_id: 1,
+    }, (error, result) => {
       // Check if result returns object genre
       // console.log(`\x1b[33m!!MUCHO IMPORTANTE ${result.genre}\x1b[0m`);
       if (error) throw error;
@@ -45,16 +53,14 @@ router.get('/user', (req, res, next) => {
       });
     });
   });
-});
 
-router.post('/user', (req, res, next) => {
-  const userBody = req.body;
-  const nameUser = req.body.name;
-  const ageUser = req.body.age;
-  const genderUser = req.body.gender;
-  const preferedGenres = req.body.movieGenre;
-  console.log(`test check: ${nameUser} likes ${preferedGenres}`);
-  db.initialize(dbName, collectionName, function(dbCollection) {// successCallback
+  router.post('/user', (req, res, next) => {
+    const userBody = req.body;
+    const nameUser = req.body.name;
+    const ageUser = req.body.age;
+    const genderUser = req.body.gender;
+    const preferedGenres = req.body.movieGenre;
+    console.log(`test check: ${nameUser} likes ${preferedGenres}`);
     dbCollection.insertOne(userBody, (error, result) => {
       if (error) throw error;
       // return user list
@@ -70,35 +76,67 @@ router.post('/user', (req, res, next) => {
         });
       });
     });
-  }, function(err) { // failureCallback
-    throw (err);
   });
-});
 
-/* retrieves specific user on _ID and parses it as a json object */
-router.get('/findUser/:_id', (req, res, next) => {
-  console.log(`Enter add/findUser/${JSON.stringify(req.params)}`);
-  const objId = new ObjectId(req.params._id);
+  /* retrieves specific user on _ID and parses it as a json object */
+  router.get('/findUser/:_id', (req, res, next) => {
+    console.log(`Enter add/findUser/${JSON.stringify(req.params)}`);
+    const objId = new ObjectId(req.params._id);
 
-  db.initialize(dbName, collectionName, function(dbCollection) {
-    dbCollection.findOne({_id: objId}, (error, result) => {
+
+    dbCollection.findOne({
+      _id: objId,
+    }, (error, result) => {
       console.log(`${JSON.stringify(result)}`);
       if (error) throw error;
       res.json(result);
     });
   });
-});
 
-/* Retrieves all users within collection 'users' */
-router.get('/allUsers', (req, res, next) => {
-  db.initialize(dbName, collectionName, function(dbCollection) {
-    dbCollection.find().toArray((error, result) =>{
+  /* Retrieves all users within collection 'users' */
+  router.get('/allUsers', (req, res, next) => {
+    dbCollection.find().toArray((error, result) => {
       if (error) throw error;
       // console.log(result); //logs all users within user
       // console.log(result.length); //prints length of users
       // res.json(result);
       res.render('./partial/user', {
         allUsers: result,
+      });
+    });
+  });
+
+  // 2-6-2020 : Currently when clicking on delete, sends user to /add/allUsers
+  router.delete('/allUsers/:id', (request, response) => {
+    const itemId = request.params.id;
+    console.log('Delete item with id: ', itemId);
+
+    dbCollection.deleteOne({id: itemId}, function(error, result) {
+      if (error) throw error;
+      // send back entire updated list after successful request
+      dbCollection.find().toArray(function(_error, _result) {
+        if (_error) throw _error;
+        response.json(_result);
+      });
+    });
+  });
+}, function(err) { // failureCallback
+  throw (err);
+});
+
+router.get('/user', (req, res, next) => {
+  console.log('Entered add/user');
+  db.initialize(dbName, 'genres', function(dbCollection) {
+    dbCollection.findOne({
+      num_id: 1,
+    }, (error, result) => {
+      // Check if result returns object genre
+      // console.log(`\x1b[33m!!MUCHO IMPORTANTE ${result.genre}\x1b[0m`);
+      if (error) throw error;
+      // res.json(result);
+      res.render('partial/addUser', {
+        name: 'Add a user to database',
+        result: result,
       });
     });
   });
