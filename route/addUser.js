b90a541a897c4d18e8f66ffd9396b70f41e6d220
Nathan <dewakik@gmail.com>
2020-06-02 21:34:39 +0200

    [UPDATE] remove unneeded package & added comments

--- route/addUser.js
+++ route/addUser.js
@@ -26,6 +26,7 @@ const ObjectId = require('mongodb').ObjectId;
 /**
  * @title mongoDB
  * See {@link dbFile}
+ * @description Initionalisation of database connection
  * @source https://dev.to/lenmorld/rest-api-with-mongodb-atlas-cloud-node-and-express-in-10-minutes-2ii1
  */
 db.initialize(dbName, collectionName, function(dbCollection) { // successCallback
