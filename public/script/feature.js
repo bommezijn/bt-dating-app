f10b0d86c92b707f23ef308cc9cbf4723fb91924
Nathan <dewakik@gmail.com>
2020-06-29 11:01:58 +0200

    [update] hide all but does not return to opacity 1

--- public/script/feature.js
+++ public/script/feature.js
@@ -16,7 +16,8 @@ function readChange(elementList) {
   for (let index = 0; index < elementList.length; index++) {
     if (elementList[index].checked === true) {
       const test1 = sexualPreference.value;
-      const test2 = userGender[index].innerHTML;
+      // const test2 = userGender[index].innerHTML;
+      const test2 = document.getElementsByClassName('userGender')[index].getElementsByTagName('span')[0].innerText;
       console.log(`test1: ${test1}, test2: ${test2}`);
       // console.log(elementList[index].defaultValue);
       // console.log(`woop: ${checkPref(test1, test2)}`);
@@ -25,23 +26,8 @@ function readChange(elementList) {
         for (let index = 0; index < users.length; index++) {
           const element = users[index];
           element.style.opacity = 0.1;
-          // console.log(`${element[index].}`);
-          if (element.querySelectorAll('.userGender').innerHTML !== test2.value) {
-            console.log('tete');
-          }
         }
       }
-    }
-  }
-}
-
-function readChangeV2(elements) {
-  for (let index = 0; index < elements.length; index++) {
-    const element = elements[index];
-    const genderSelector = element.querySelectorAll('.userGender');
-
-    if (genderSelector == true) {
-      console.log('dsdsds')
     }
   }
 }
